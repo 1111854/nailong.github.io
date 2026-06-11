49c2c1594e424c098db6a30b807123e1ae68c28f
1111854 <202500460034@mail.sdu.edu.cn>
2026-06-11 21:00:03 +0800

    Update nl.py

--- nl.py
+++ nl.py
@@ -21,7 +21,8 @@ API_URL = "https://mynewapi.n1neman.fun/v1"
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 HISTORY_DIR = os.path.join(BASE_DIR, "chat_history")
 UPLOAD_DIR = os.path.join(BASE_DIR, "uploads")
-
+if os.path.exists(gif_path):
+        st.image(gif_path)
 for dir_path in [HISTORY_DIR, UPLOAD_DIR]:
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
